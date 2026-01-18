9e6abb7696a078d24ae5ac6956d3a2d0d600fed7
Nitishroy1 <nitishk9199@gmail.com>
2026-01-18 13:15:37 +0530

    Add some String in LinkedList

---
+++ LinkedList/com/linkedlistdemo/LinkedlistDemo2.java
@@ -0,0 +1,14 @@
+package com.linkedlistdemo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedlistDemo2 {
+	void main() {
+		List<String> list=new LinkedList<>();
+		list.add("Nitish Kumar");
+		list.add("Rajnish");
+		list.add("Ram");
+	}
+
+}
