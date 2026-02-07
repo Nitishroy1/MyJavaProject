0671f11e8d32cd897e2acbaca94833a669a40948
Nitishroy1 <nitishk9199@gmail.com>
2026-02-07 23:16:24 +0530

    Stream api

--- Stream Api/com/Arrays/Strime/ArraysDemo.java
+++ Stream Api/com/Arrays/Strime/ArraysDemo.java
@@ -1,6 +1,8 @@
 package com.Arrays.Strime;
 
 import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ArraysDemo 
 {
@@ -69,5 +71,13 @@ public class ArraysDemo
 	                return word.equals(rev.toString()); // FIX
 	            })
 	            .forEach(System.out::println);
+	        
+	        
+	        Integer[] numbers = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+	        Set<Integer> set = Arrays.stream(numbers)
+	           .filter(n -> n%2==0)
+	              .collect(Collectors.toSet());
+	            set.forEach(IO::println);
+	            IO.print(set);
 	    }
 }
